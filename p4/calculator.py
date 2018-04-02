e7562e442b5ff0f5c4d51c1e4ca025123ed0eeb8
cdjay <76025226@qq.com>
2018-04-02 21:59:05 +0800

    P4重构

--- p4/calculator.py
+++ p4/calculator.py
@@ -1,154 +1,205 @@
 # !usr/bin/env python3
-# ! -*- coding:utf8 -*-
 
 import sys
 import csv
-# from multiprocessing import Process, Queue
+import queue
+from multiprocessing import Process, Queue
 
-# 参数处理 (  tax:税率 user_data:用户薪资 out_file:输出 city:城市  )
+
+# 甩出错误
+class ArgError(Exception):
+    pass
+
+
+# 参数处理
 class Args:
 
     # 初始化
-    def __init__(self):
-        # 获取命令行列表
-        self.args = sys.argv[1:]
-        # 处理参数
-        self._get_parameter()
+    def __init__(self, args):
+        # 获取命令行参数
+        self.args = args
 
-    # 处理参数函数
-    def _get_parameter(self):
-        # 参数字典
-        arglist = {
-            "-C": "所在城市名称",
-            "-c": "税率配置文件",
-            "-d": "员工薪资原始文件",
-            "-o": "生成工资条文件",
-            "-h": "Usage: calculator.py -C cityname -c configfile -d userdata -o resultdata",
-            "--help": "Usage: calculator.py -C cityname -c configfile -d userdata -o resultdata"
-        }
+    # 处理
+    def __parse_arg(self, arg):
+        try:
+            value = self.args[self.args.index(arg) + 1]
+        except(ValueError, IndexError):
+            value = None
+        return value
 
-        if "-h" in self.args or "--help" in self.args:
-            print(arglist['-h'])
-            return
+    # 获取路径
+    def get_arg(self, arg):
+        value = self.__parse_arg(arg)
+
+        if value is None:
+            raise ArgError('Not found arg {}'.format(arg))
+        return value
+
+
+# 读取社保配置
+class SheBaoConfig:
+
+    def __init__(self, file):
+
+        self.jishu_low, self.jishu_high, self.rate = self.__parse_config(file)
+
+    def __parse_config(self, file):
+
+        rate = 0
+        jishu_low = 0
+        jishu_high = 0
+
+        with open(file) as f:
+            for line in f:
+                key, value = line.split('=')
+                key = key.strip()
+                try:
+                    value = float(value.strip())
+                except ValueError:
+                    continue
+                if key == 'JiShuL':
+                    jishu_low = value
+                elif key == 'JiShuH':
+                    jishu_high = value
+                else:
+                    rate += value
+        return jishu_low, jishu_high, rate
+
+
+class StaffData(Process):
+
+    def __init__(self, file, output_queue):
+
+        self.file = file
+        self.output_q = output_queue
+        super().__init__()
+
+    @property
+    def __parse_data(self):
+        # 一次一行
+        for line in open(self.file):
+            staff_id, gongzi = line.split(',')
+            yield (int(staff_id), int(gongzi))
+
+    def run(self):
+
+        for item in self.__parse_data:
+            self.output_q.put(item)
+
+
+class Calculator(Process):
+
+    #  起征点
+    tax_start = 3500
+
+    # 速查表(应纳税额,税率,速算扣除数)
+    tax_table = [
+        (80000, 0.45, 13505),
+        (55000, 0.35, 5505),
+        (35000, 0.3, 2755),
+        (9000, 0.25, 1005),
+        (4500, 0.2, 555),
+        (1500, 0.1, 105),
+        (0, 0.03, 0),
+    ]
+
+    def __init__(self, config, input_queue, out_queue):
+
+        self.config = config
+        self.input_q = input_queue
+        self.output_q = out_queue
+
+        # 手动调用__init__
+        super().__init__()
+
+    def calculate(self, data_item):
+
+        employee_id, gongzi = data_item
+
+        if gongzi < self.config.jishu_low:
+            shebao = self.config.jishu_low * self.config.total_rate
+        elif gongzi > self.config.jishu_high:
+            shebao = self.config.jishu_high * self.config.total_rate
         else:
+            shebao = gongzi * self.config.rate
+
+        # 参与计算
+        left_gongzi = gongzi - shebao
+
+        # 应纳税所得额
+        tax_gongzi = left_gongzi - self.tax_start
+
+        # 判断是否有个税
+        if tax_gongzi < 0:tax = 0
+        else:
+            for item in self.tax_table:
+                if tax_gongzi > item[0]:
+                    tax = tax_gongzi * item[1] - item[2]
+                    break
+
+        # 最终工资
+        last_gongzi = left_gongzi - tax
+
+        return str(employee_id), str(gongzi),'{:.2f}'.format(shebao),'{:.2f}'.format(tax),'{:.2f}'.format(last_gongzi)
+
+    def run(self):
+        # 循环获取数据,指定超时时间,如果超时则推出
+        while True:
+            try:item = self.input_q.get(timeout=1)
+            except queue.Empty:return
+            # 将结果放到队列中
+            result = self.calculate(item)
+            self.output_q.put(result)
+
+class Exporter(Process):
+
+    def __init__(self, file, input_queue):
+
+        self.file = open(file, 'w')
+        self.input_q = input_queue
+        # 手动调用__init__
+        super().__init__()
+
+    def export(self, item):
+        line = ','.join(item) + '\n'
+        self.file.write(line)
+
+    def close(self):
+        self.file.close()
+
+    def run(self):
+        while True:
             try:
-                self.tax = self.args[self.args.index('-c') + 1]
-                self.data = self.args[self.args.index('-d') + 1]
-                self.result = self.args[self.args.index('-o') + 1]
-                # self.city = self.args[self.args.index('-C') + 1]
-            except:
-                print("Error: 没有输入足够的参数 请输入 [-h] 或者 [--help] 查看使用帮助 ")
-
-# 获取税率配置 (  config 字典  )
-class Tax:
-
-    def __init__(self, path):
-        # 读取配置
-        self.config = self._read_config(path)
-
-    def _read_config(self, path):
-        config = {}
-        try:
-            with open(path, 'r') as f:
-                for x in f:
-                    dict = x.split("=")
-                    config[dict[0].strip()] = float(dict[1].strip())
-        except:
-            print("Error:{} hava a some problem! Please check it!".format(path))
-        finally:
-            return config
-
-    def get(self, name):
-        return self.config[name]
-
-# 获取用户原始数据 (  data 字典  )
-class Staff:
-
-    def __init__(self, path):
-        self.data = self._read_staffdata(path)
-
-    def _read_staffdata(self, path):
-        data = {}
-        try:
-            with open(path, 'r') as f:
-                for x in f:
-                    dict = x.split(",")
-                    data[dict[0].strip()] = float(dict[1].strip())
-        except:
-            print("Error:{} hava a some problem! Please check it!".format(path))
-        finally:
-            return data
-
-# 计算工资
-class IncomeTaxCalculator:
-
-    # 初始化
-    def __init__(self):
-        self.ladder =[
-            [80000, 0.45, 13505],
-            [55000, 0.35, 5505],
-            [35000, 0.3, 2755],
-            [9000, 0.25, 1005],
-            [4500, 0.2, 555],
-            [1500, 0.1, 105],
-            [0, 0.03, 0]
-        ]
-    # 社保金额
-    def YNSuode(self, pay):
-
-        taxpay=pay
-        # 社保计算基数
-        if pay >= tax.get('JiShuH'):taxpay=tax.get('JiShuH')
-        if pay <= tax.get('JiShuL'): taxpay = tax.get('JiShuL')
-
-        # 社保费率
-        soctax=tax.get('YangLao')
-        soctax+= tax.get('YiLiao')
-        soctax += tax.get('ShiYe')
-        soctax += tax.get('GongShang')
-        soctax += tax.get('ShengYu')
-        soctax += tax.get('GongJiJin')
-
-        return taxpay*soctax
-
-    # 个税金额
-    def GS(self, pay,sbje):
-        # 计算应征额度
-        shui=pay-sbje-3500
-
-        if shui <=0:shui=0
-        for num in range(len(self.ladder)):
-            dangci,shuilv,susuan=self.ladder[num][0],self.ladder[num][1],self.ladder[num][2]
-            if shui >= dangci:
-                return shui*shuilv-susuan
-
-    # 计算并输出
-    def export(self):
-        with open(path.result,'w',newline='') as file:
-            csv_writer=csv.writer(file)
-            for name, pay in staff.data.items():
-                # 社保金额
-                sbje=self.YNSuode(pay)
-                # 个税金额 传入工资,社保)
-                gsje=self.GS(pay,sbje)
-                out="{},{:.2f},{:.2f},{:.2f},{:.2f}".format(name,pay,sbje,gsje,pay-sbje-gsje).split(',')
-                csv_writer.writerow(out)
+                # 循环获取数据,指定超时时间,如果超时则推出
+                item = self.input_q.get(timeout=1)
+            except queue.Empty:
+                self.close()
+                return
+            self.export(item)
 
 
+# 执行
 if __name__ == "__main__":
-    # 开进程
-    # queue = Queue(maxsize=3)
+    # 读取参数
+    path = Args(sys.argv[1:])
+    # 读取社保
+    config = SheBaoConfig(path.get_arg('-c'))
 
-    # 数据处理
-    try:
-        # 处理参数
-        path = Args()
-        # 读取税率文件
-        tax = Tax(path.tax)
-        # 读取员工数据
-        staff = Staff(path.data)
-    except:
-        print("读取文件发生未知错误,请检查数据格式是否正确!")
+    queue1 = Queue()
+    queue2 = Queue()
 
-    gongzi=IncomeTaxCalculator()
-    gongzi.export()
+    # 读取员工
+    staff_data = StaffData(path.get_arg('-d'), queue1)
+    # 计算工资
+    calculator = Calculator(config, queue1, queue2)
+    # 导出文件
+    exporter = Exporter(path.get_arg('-o'), queue2)
+
+    # 启动
+    staff_data.start()
+    calculator.start()
+    exporter.start()
+
+    staff_data.join()
+    calculator.join()
+    exporter.join()
