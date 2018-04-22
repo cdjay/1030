62ee5917ea6a50d27d9414ba6cf2ecbc8435a798
cdjay <76025226@qq.com>
2018-04-22 14:38:25 +0800

    update

--- p6/news/app.py
+++ p6/news/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import render_template,abort
 
 app=Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'),404
 
 @app.route('/')
 def index():
