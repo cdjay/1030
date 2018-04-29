2c45426531f7d85d5bc09d0541a5f80ef2a4f5e6
cdjay <76025226@qq.com>
2018-04-30 02:16:17 +0800

    p6 for flask

--- p6/news/app.py
+++ p6/news/app.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from flask import render_template as render
+import os
+import json
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    pass
+    return os.walk('../files/')
 
 @app.route('/files/<filename>')
 def file(filename):
-    pass
+    filename+=".json"
+    if os.path.isfile(filename):
+        return render("404.html")
+    else:
+        return "Find the file"
 
 
